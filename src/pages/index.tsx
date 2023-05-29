c0ce5bb0d700b1244a876688192d1d53caef12af
rarandeyo <s1f102301392@iniad.org>
2023-05-29 22:58:35 +0900

    add new file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,14 +37,51 @@ const Home = () => {
   [-1,-1,-1,-1,-1,-1,-1,-1,-1],
   [-1,-1,-1,-1,-1,-1,-1,-1,-1],
 ]
-//-1 ->石
+  //-1 ->石
   //0 ->画像なしセル
   //1~8 ->数字セル
   //9 ->石＋はてな
   //10 ->石＋旗
   //11 ->ボムセル
 
+  const directions = [
+    { dx: 0, dy: -1 }, // 上
+    { dx: 0, dy: 1 }, // 下
+    { dx: -1, dy: 0 }, // 左
+    { dx: 1, dy: 0 }, // 右
+    { dx: -1, dy: -1 }, // 左上
+    { dx: 1, dy: -1 }, // 右上
+    { dx: -1, dy: 1 }, // 左下
+    { dx: 1, dy: 1 }, // 右下
+  ];
   
+
+  //押した場所の周りの爆弾の数を数えて配列に代入する関数
+  const count_bomb = (a : number, b : number) => {
+  let bomb_counter = 0;
+
+  for (const { dx, dy } of directions) {
+    
+    const nx = a + dx;
+    const ny = b + dy;
+    if (bomb_board[ny] !== undefined){
+      if (bomb_board[ny][nx] === 1){
+      bomb_counter++;
+      }
+    }
+  };
+  return bomb_counter
+  };
+  
+  //爆弾を数える関数を全部のマスでやる
+  for (let i = 1;i<9;i++){
+    for (let j = 1;j<9;j++){
+      count_bomb(i,j)
+
+    }
+  }
+
+
   
   const clickLine = (x: number, y: number) => {
     console.log(x, y);
@@ -61,7 +98,7 @@ const Home = () => {
     }
     if (counter === 0) {
       const userBoardCopy: number[][] = JSON.parse(JSON.stringify(user_board));
-      let zeroPositions = [];
+      const zeroPositions = [];
       for (let i = 0; i < userBoardCopy.length; i++) {
         for (let j = 0; j < userBoardCopy[i].length; j++) {
           if (userBoardCopy[i][j] === 0) {
@@ -72,16 +109,16 @@ const Home = () => {
       if (zeroPositions.length >= 10) {
         const bombBoardCopy: number[][] = JSON.parse(JSON.stringify(bomb_board));
         for (let i = 0; i < 10; i++) {
-          let randomIndex = Math.floor(Math.random() * zeroPositions.length);
-          let [row, col] = zeroPositions[randomIndex];
+          const randomIndex = Math.floor(Math.random() * zeroPositions.length);
+          const [row, col] = zeroPositions[randomIndex];
           bombBoardCopy[row][col] += 1;
           zeroPositions.splice(randomIndex, 1); // remove used position
         }
         bomb_setboard(bombBoardCopy);
       }
     }
-  };
 
+  };
 
 
   
@@ -91,15 +128,10 @@ const Home = () => {
 return (
     <div className={styles.container}>
       <div className={styles.square}>
-        {user_board.map((raw, y) =>
+        {bomb_board.map((raw, y) =>
           raw.map((cell_number, x) => (
             <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickLine(x, y)}
-            style={{backgroundPosition: -30*cell_number+30 }}>
-
-
-
-              
-            </div>
+            style={{backgroundPosition: -30*cell_number+30 }} />
           ))
         )}
       </div>
