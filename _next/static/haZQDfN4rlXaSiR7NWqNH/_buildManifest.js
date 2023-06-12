0853508503fbb16521d87745d15384ff3e9ebed4
rarandeyo <rarandeyo@users.noreply.github.com>
2023-06-12 13:12:39 +0000

    deploy: ee5e6d2480b4bb1d0dedb46022d3cb86c4312bb9

---
+++ _next/static/haZQDfN4rlXaSiR7NWqNH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b88a6d653ba57006.css","static/chunks/pages/index-0318f98bc783363f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
